f45db15c1efcec7b908016c4524a20f7a8dcd5b6
Sara.Lignell <srl84@georgetown.edu>
2024-11-06 20:13:29 -0500

    updates to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,23 +13,27 @@ const Header = () => (
 
 const styles = StyleSheet.create({
   header: {
-    height: 70, // Standard height for a news app header
+    height: 90, // Standard height for a news app header
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'center', // Center logo
     backgroundColor: '#094C9F', // Blue background
     paddingHorizontal: 16, // Standard horizontal padding
     paddingBottom: 5,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0, // Handle Android StatusBar
+    paddingTop: 40,
     
   },
   logo: {
     width: 120, // Standard logo width
     height: 40, // Standard logo height
     resizeMode: 'contain', // Ensures logo scales well
+   
   },
   icon: {
-    padding: 8, // Adjust touchable area for better UX
+    position: 'absolute', // Position search icon independently
+    right: 16, // Align to the right
+    paddingRight: 8, // Adjust touchable area for better UX
+    paddingTop: 40,
   },
 });
 
